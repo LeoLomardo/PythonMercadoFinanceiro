00769bb9fd6e7e0a4ef3fc39bb30dc8ebec0f917
Leo Lomardo <leoland771@gmail.com>
2022-08-23 18:20:59 -0300

    Update fronteira_eficiencia.py

--- fronteira_eficiencia.py
+++ fronteira_eficiencia.py
@@ -2,41 +2,26 @@
 import pandas as pd
 import matplotlib.pyplot as fig
 import numpy as np
-#precisei instalar sudo apt-get install python3-tk
+# precisei instalar sudo apt-get install python3-tk
 import scipy.optimize as solver
 import pandas_datareader.data as web
 import datetime as dt
-#++++++++++++++++++++++++++++++++++++++++++++ ( MONTA EXEL COM AÇÕES ) ++++++++++++++++++++++++++++++++++++++++++++++++#
-
-#DELIMITANDO ESPACO DE TEMPO
+# ++++++++++++++++++++++++++++++++++++++++++++ ( MONTA EXEL COM AÇÕES ) ++++++++++++++++++++++++++++++++++++++++++++++++#
 inicio = dt.datetime(2015, 1, 2)
 fim = dt.datetime(2020, 11, 3)
+list = ['GGBR4.SA', 'BBDC4.SA','EMBR3.SA', 'BOVA11.SA']
 
-#VETOR COM NOME DOS ATIVOS
-list = ['GOLL4.SA', 'CVCB3.SA', 'WEGE3.SA', 'MGLU3.SA', 'TOTS3.SA', 'BOVA11.SA']
-
-len(list)
-#MONTANDO MATRIZ COM VALORES, CASO QUERIA MAIS INFO, DELETAR "['Adj Close']", CASO QUEIRA SO PRECO DE FECHAMENTO, MANTER
 df = web.DataReader(list, 'yahoo', inicio, fim)['Adj Close']
-
-#ESCREVE NO ARQUIVO EXEL DESTINO
 writer = pd.ExcelWriter('Teste3.xlsx', engine='xlsxwriter')
-
-#ESCOLHE TABELA QUE IMPRIME OS DADOS
 df.to_excel(writer, sheet_name='Sheet1')
-
-#SALVA
 writer.save()
 
 
 #+++++++++++++++++++++++++++++++++++++++++++++ ( OTIMIZAÇÃO DA CARTEIRA ) +++++++++++++++++++++++++++++++++++++++++++++#
 
 df = pd.read_excel('Teste3.xlsx', sheet_name='Sheet1')
-
 fig.style.use('ggplot')
-
 fig.figure()
-
 ax1 = fig.subplot(111)
 ax1.plot(df['Date'], df[list], '-k', alpha=0.8)
 ax1.legend(list)
@@ -49,22 +34,20 @@ n = len(df)
 prec = df.drop(['Date'], axis=1)
 
 ri = prec/prec.shift(1)-1
-mi = ri.mean()*len(df) #esses valores precisam estar corretos, referentes ao numero de linhas da tabela exel
+mi = ri.mean()*len(df)
 sigma = ri.cov()*len(df)
-print(' ')
-print(' ')
 print('++++++++++++++++++++++Matriz de Covariância da carteira++++++++++++++++++++++')
 print(sigma)
 
 vet_R = []
 vet_Vol = []
 
-for i in range(2000): #com minha carteira inicial, colocar 2000 ou 30000 passos nao fez diferenca significativo, testar para valores maiores
+for i in range(2000): # com minha carteira inicial, colocar 2000 ou 30000 passos nao fez diferenca significativo, testar para valores maiores
     # aqui voce define o peso da alocao de cada ativo da carteira
     w = np.random.random(len(list))
     w = w/np.sum(w)
     # outro exemplo de alocar
-    #w =np.array([0.19524, 0.36, 0.028913, 0.1591, 0.256747])  #LEMBRANDO QUE O TOTAL PRECISA SER 1
+    # w =np.array([0.19524, 0.36, 0.028913, 0.1591, 0.256747])  #LEMBRANDO QUE O TOTAL PRECISA SER 1
 
     retorno = np.sum(w*mi)
     risco = np.sqrt(np.dot(w.T, np.dot(sigma, w)))
@@ -86,48 +69,50 @@ ax2.set_ylabel("RETORNO ESPERADO", fontsize=14)
 fig.title('Retorno Esperado x Volatilidade esperada', fontsize=10)
 fig.show()
 
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++( FRONTEIRA EFICIENTE) ++++++++++++++++++++++++++++++++++++++++++++++++++++#
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++( FRONTEIRA EFICIENTE) ++++++++++++++++++++++++++++++++++++++++++++++++++++#
 
 
 def f_obj(peso):
-    return np.sqrt(np.dot(peso.T, np.dot(sigma,peso)))
+    return np.sqrt(np.dot(peso.T, np.dot(sigma, peso)))
 
 
-x0 = np.array([1.0/ (len(list)) for x in range(len(list))])
+x0 = np.array([1.0/(len(list)) for x in range(len(list))])
 
-bounds = tuple((0, 1) for x in range (len(list)))
+bounds = tuple((0, 1) for x in range(len(list)))
 
 faixa_ret = np.arange(0.001, 2.5, 0.001)
 
 risk = []
 
 for i in faixa_ret:
-    constrains = [{'type' : 'eq', 'fun' : lambda x: np.sum(x) - 1}, {'type' : 'eq', 'fun' : lambda x: np.sum(x * mi) - i}]
-    outcome = solver.minimize(f_obj, x0, constraints=constrains,bounds=bounds , method='SLSQP')
+    constrains = [{'type' : 'eq', 'fun' : lambda x: np.sum(x) - 1}, {'type' : 'eq', 'fun': lambda x: np.sum(x * mi) - i}]
+    outcome = solver.minimize(f_obj, x0, constraints=constrains, bounds=bounds, method='SLSQP')
 
     risk.append(outcome.fun)
 
-fig.plot(risk, faixa_ret, 'r--x', linewidth = 5)
+fig.plot(risk, faixa_ret, 'r--x', linewidth=5)
 
 print('')
 print('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
-print('                 GOLL4.SA CVCB3.SA WEGE3.SA MGLU3.SA TOTS3.SA BOVA11.SA')
-print('peso ótimo(w) = ', outcome['x'].round(9)) #numero 6 é referente ao aerredondamento de 6 casas decimais
+
+print('peso otimo(w) = ', outcome['x'].round(9)) # numero 6 é referente ao aerredondamento de 6 casas decimais
 print('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
 print('')
 
 #fig.show()
 
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++( PONTO ÓTIMO - MÍNIMA VOLATILIDADE) ++++++++++++++++++++++++++++++++++++++++++++++++++++#
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++( PONTO OTIMI - MINIMA VOLATILIDADE) ++++++++++++++++++++++++++++++++++++++++++++++++++++#
 
 
 def estatistica_port(peso):
     peso = np.array(peso)
     ret_ot = np.sum(peso*mi)
-    risco_ot = np.sqrt(np.dot(peso.T, np.dot(sigma,peso)))
+    risco_ot = np.sqrt(np.dot(peso.T, np.dot(sigma, peso)))
     return np.array([ret_ot, risco_ot])
+
+
 for i in faixa_ret:
-    constrains = [{'type' : 'eq', 'fun' : lambda x: sum(x) - 1}]
+    constrains = [{'type' : 'eq', 'fun': lambda x: sum(x) - 1}]
     outcome = solver.minimize(f_obj, x0, constraints=constrains, bounds=bounds, method='SLSQP')
 
     risk.append(outcome.fun)
@@ -135,5 +120,5 @@ for i in faixa_ret:
 ret_ot, vol_ot = estatistica_port(outcome['x'])
 print('retorno otimo esperado = ', str((ret_ot*100).round(len(list))) + '%')
 print('volatilidade otima esperada = ', str((vol_ot*100).round(len(list))) + '%')
-fig.plot(vol_ot, ret_ot, '*', markersize=35, markerfacecolor = 'w', markeredgecolor ='black')
+fig.plot(vol_ot, ret_ot, '*', markersize=35, markerfacecolor='w', markeredgecolor='black')
 fig.show()
